b383afebbf99b5c9e9e1d9412c42d93ceae49b44
lopertut <artur.matvejev12345@gmail.com>
2024-07-24 21:24:54 +0300

    postgresql database connected

--- Store/settings.py
+++ Store/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Store',
+        'USER': 'postgres',
+        'PASSWORD': 'boom1212',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
