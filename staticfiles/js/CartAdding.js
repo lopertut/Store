d40a67a9c9015298e7bb37d43cd1a16b7adecf7b
lopertut <artur.matvejev12345@gmail.com>
2024-10-20 17:13:14 +0300

    added dynamic cart update

--- staticfiles/js/CartAdding.js
+++ staticfiles/js/CartAdding.js
@@ -1,7 +1,5 @@
 function HandleAdding(product_id, quantity) {
     const csrftoken = getCookie('csrftoken');
-    console.log("CSRF Token: ", csrftoken);
-    console.log("id:", product_id, "quantity:", quantity);
 
     fetch(addToCartUrl, {
         method: 'POST',
@@ -14,14 +12,9 @@ function HandleAdding(product_id, quantity) {
             'quantity': quantity
         })
     })
-        .then(response => response.json())  // Ожидаем JSON в ответе
+        .then(response => response.json())
         .then(data => {
-            console.log('Response from server:', data);  // Логируем ответ
-            if (data.success) {
-                alert("Product added");
-            } else {
-                alert('Failed to add product to cart: ' + data.error);  // Логируем ошибку
-            }
+            console.log('Response from server:', data);
         })
         .catch((error) => console.error('Error:', error));
 }
