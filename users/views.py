3ceefbd8e9223f63af77d5c882208325db76b33d
lopertut <artur.matvejev12345@gmail.com>
2024-10-25 20:32:20 +0300

    moved authentication to new users app

---
+++ users/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from users.authorization import CreateUserForm, LoginForm
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import auth
+
+
+def login(request):
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect('shop/index')
+
+    context = {
+        'login_form': form
+    }
+
+    return render(request, 'login_form.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
+
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {
+        'registration_form': form
+    }
+
+    return render(request, 'registration_form.html', context=context)
