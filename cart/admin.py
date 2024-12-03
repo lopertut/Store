652fdd7ef0f187297368129db3545f3045c33753
lopertut <artur.matvejev12345@gmail.com>
2024-12-03 17:38:08 +0200

    updated cart admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import *
+from .models import Cart, CartItem
 
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ('user', 'product', 'quantity')
-    list_filter = ('user', 'product')
-    search_fields = ('user__username', 'product__name')
+    list_display = ('id', 'user', 'created_date')
+    list_filter = ('user',)
+    search_fields = ('user__username',)
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity')
+    list_filter = ('cart', 'product')
+    search_fields = ('cart__user__username', 'product__name')
