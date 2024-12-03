ee2a8b75c51e70f07bb1b41037cf7e710cb88045
lopertut <artur.matvejev12345@gmail.com>
2024-12-03 17:35:01 +0200

    moved info about products to catalog admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import Products
 
-# Register your models here.
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stock', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name',)
+    ordering = ('name',)
